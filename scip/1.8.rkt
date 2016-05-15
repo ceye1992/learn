bfc40809fec0db223b95ab4e5f1fb16182160690
ceye1992 <691092546@qq.com>
2016-05-15 20:43:24 +0800

    sicp

---
+++ scip/1.8.rkt
@@ -0,0 +1,21 @@
+#lang racket
+(define (cube-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (cube-iter (improve guess x)
+                 x)))
+(define (improve guess x)
+  (/ (+ (/ x
+           (* guess guess))
+        (* 2 guess))
+     3))
+(define (good-enough? guess x)
+  (< (abs(- (* guess guess guess)
+            x))
+     0.001))
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
+(define (cube x)
+  (cube-iter 1.0 x))
