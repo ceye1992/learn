2a35787b92fdca8847baadb930a3f7d2c5a1b1fc
ceye1992 <691092546@qq.com>
2016-06-16 21:15:50 +0800

    sicp

---
+++ sicp/2.42.rkt
@@ -0,0 +1,55 @@
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low
+         (enumerate-interval (+ low 1) high))))
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+(define (accumulate op initerval sequence)
+  (if (null? sequence)
+      initerval
+      (op (car sequence)
+          (accumulate op initerval (cdr sequence)))))
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else
+         (filter predicate (cdr sequence)))))
+
+
+(define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter
+         (lambda (positions) (safe? k positions))
+         (flatmap
+          (lambda (rest-of-queens)
+            (map (lambda (new-row)
+                   (adjoin-position new-row k rest-of-queens))
+                 (enumerate-interval 1 board-size)))
+          (queen-cols (- k 1))))))
+  (queen-cols board-size))
+
+(define empty-board '())
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cons new-row rest-of-queens))
+
+(define (safe? k position)
+  (iter-check (car position)
+              (cdr position)
+              1))
+(define (iter-check row-of-new-queen rest-of-queens i)
+  (if (null? rest-of-queens)
+      #t
+      (let ((row-of-current-queen (car rest-of-queens)))
+        (if (or (= row-of-current-queen row-of-new-queen)
+                (= row-of-new-queen (+ i row-of-current-queen))
+                (= row-of-new-queen (- row-of-current-queen i)))
+            #f
+            (iter-check row-of-new-queen
+                        (cdr rest-of-queens)
+                        (+ i 1))))))
