a4558c4df09132f7d9f5de34d5d74e249324c14f
ceye1992 <691092546@qq.com>
2016-07-04 21:42:32 +0800

    sicp

---
+++ sicp/2.74b.rkt
@@ -0,0 +1,42 @@
+
+(define (deriv exp var)
+  (cond ((munber? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        (else ((get 'deriv (operator exp)) (operands exp)
+                                           var))))
+
+(define (operator exp)
+  (car exp))
+(define (operands exp)
+  (cd exp))
+
+(define (install-sum-package)
+  (define (addend s)
+    (car s))
+  (define (augend s)
+    (cadr s))
+  (define (make-sum x y)
+    (cond ((=number? x 0)
+           y)
+          ((=number? y 0)
+           x)
+          ((and (number? x) (number? y))
+           (+ x y))
+          (else
+           (sttach-tag '+ x y))))
+
+  (put 'addend '+ addend)
+  (put 'augend '+ augend)
+  (put 'make-sum '+ make-sum)
+  (put 'deriv '+
+       (lambda (exp var)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var))))
+  'done)
+
+(define (make-sum x y)
+  ((get 'make-sum '+) x y))
+(define (addend sum)
+  ((get 'addend '+) (contents sum)))
+(define (augend sum)
+  ((get 'augend '+) (contents cum)))
