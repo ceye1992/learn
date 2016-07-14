c6478a8745a307b30c3b0adcf1ae47f3981fd317
ceye1992 <691092546@qq.com>
2016-07-14 18:20:58 +0800

    sicp

---
+++ sicp/3.23.rkt
@@ -0,0 +1,69 @@
+(define (front-ptr deque)
+  (car deque))
+(define (rear-ptr deque)
+  (cdr deque))
+
+(define (set-front-ptr! deque item)
+  (set-car! deque item))
+(define (set-rear-ptr! deque item)
+  (set-cdr! deque item))
+
+(define (make-deque)
+  (cons '() '()))
+
+(define (empty-deque? deque)
+  (null? (front-ptr deque)))
+
+(define (front-deque deque)
+  (if (empty-deque? deque)
+      (error "FRONT-DEQUE called with an empty deque" deque)
+      (car (front-ptr deque))))
+
+(define (rear-deque deque)
+  (if (empty-deque? deque)
+      (error "REAR-DEQUE cadded with an empty deque" deque)
+      (cdr (rear-ptr deque))))
+
+(define (insert-rear-deque! deque item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-deque? deque)
+           (set-front-ptr! deque new-pair)
+           (set-rear-ptr! deque new-pair)
+           deque)
+          (else
+           (set-cdr! (rear-ptr deque) new-pair)
+           (set-rear-ptr! deque new-pair)
+           deque))))
+
+(define (delete-front-deque! deque)
+  (cond ((empty-deque! deque)
+         (error "DELETE-FRONT-DEQUE! called with an empty deque" deque))
+        (else
+         (set-front-ptr! deque (cdr (front-ptr deque)))
+         deque)))
+
+(define (insert-front-deque! deque item)
+  (cond ((empty-deque? deque)
+         (instert-rear-deque! deque item))
+        (else
+         (set-front-ptr! deque (cons item (front-ptr deque)))
+         deque)))
+
+(define (delete-rear-deque! deque)
+  (define (iter deque lst)
+    (cond ((null? (cdr (cdr lst)))
+           (set-cdr! lst '())
+           (set-rear-ptr! deque lst)
+           deque)
+          (else
+           (iter deque (cdr lst)))))
+  (cond ((empty-deque? deque)
+         (error "DELETE-REAR-DEQUE! called with an empty deque" deque))
+        ((null? (cdr (front-ptr deque)))
+         (set-front-ptr! deque '())  ;长度等于1
+         deque)
+        (else
+         (iter deque (front-ptr deque)))))  ;长度大于1
+
+(define (print-deque deque)
+  (car deque))
